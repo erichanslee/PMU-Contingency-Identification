b564086f57b5f791f7dc23d1e7a6617cad132755
Eric Lee <ericlee0803@yahoo.com>
2017-01-20 13:06:39 -0500

    Modified LS fit a bit

--- Routines/LSfit.m
+++ Routines/LSfit.m
@@ -1,19 +1,31 @@
+% LSfit calculates a least squares fit of a DAE to a signal by forming the
+% matrix M consisting of elements (e^lambda*t)*v where (lambda, v) are
+% eigenpairs of the DAE. 
+
+% So LSfit first calculates the eigenpairs (lambda, v), forms the matrix M,
+% and then calculates c = M\S where S is the signal and c is a vector of
+% coefficients
+
 function results = LSfit(obj, contignum)
 
-% Get Metadata
+% Get Metadata and initialize some variables
 load metadata.mat
 signal = obj.dynamic_data;
 signal = signal'; % signal currently in column format
 tstep = .05;
-dataoffset = 50*timestep;
+dataoffset = 100*timestep;
 results = zeros(1, numcontigs);
+n = differential;
 
-k = contignum;
-% Grab model, form Schur Complement and calc necessary eigenpairs
-[A,~] = obj.retrieveModel(k);
+% Grab model, form Schur Complement SCH
+[A,~] = obj.retrieveModel(contignum);
+A11 = A(1:n, 1:n); A12 = A(1:n, (n+1):end);
+A21 = A((n+1):end, 1:n); A22 = A((n+1):end, (n+1):end);
+SCH = A11 - A12*(A22\A21);
 
-[vecs, vals] = eig(full(A), E);
-vecs = vecs(obj.PMU, :);
+% Calc and Filter Eigenpairs
+[vecs, vals] = eig(full(SCH));
+vecs = vecs(obj.PMU - n, :);
 vals = diag(vals);
 vals(abs(vals) < 1e-8) = 0;
 mode = 'freq';
@@ -23,9 +35,9 @@ mode = 'damp';
 
 % Form matrix M
 [len, numsteps] = size(signal);
-S = reshape(signal, [], 1);
+SIG = reshape(signal, [], 1);
 nummodes = length(fvals);
-M = zeros(length(S), nummodes);
+M = zeros(length(SIG), nummodes);
 for i = 1:numsteps
     for j = 1:nummodes
         foffset = (i-1)*len + 1;
@@ -36,9 +48,9 @@ for i = 1:numsteps
 end
 
 % Regress and Calc Residual
-c = M\S;
-res = M*c - S;
-results(k) = norm(res);
+c = M\SIG;
+res = M*c - SIG;
+results = norm(res);
 
 
 
--- Routines/tests/testLS.m
+++ Routines/tests/testLS.m
@@ -1,7 +1,10 @@
-function testLS(contignum)
+function results = testLS(contignum)
 % Tests Least Squares Fitting LSfit()
 load metadata.mat
-PMUidx = [16];
+PMUidx = 16;
 PMU = place_PMU(contignum, PMUidx);
 test = loadProblem('39bus', contignum, 'Weighted', 'Weighted', PMU);
-results = LSfit(test, contignum)
+results = zeros(1, numcontigs);
+for i = 1:numcontigs
+    results(i) = LSfit(test, i);
+end
